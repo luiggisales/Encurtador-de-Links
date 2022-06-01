0a444020a20dda5a071cfc47b38ec20c77dcbe2c
luiggi.sales.silva07 <luiggi.sales.silva07@aluno.ifce.edu.br>
2022-06-01 18:47:33 -0300

    link do app mobile

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom'
 export default function Menu() {
   return (
     <div className='Menu'>
-        <a href="https://www.instagram.com/luiggi_sales/" className='social' target={'_blank'}>
+        <a className='appmobile' href='https://www.mediafire.com/file/7fuidj98sdcgt18/EncurtadorDeLinks.apk/file'
+        target={'_blank'} rel="noreferrer">Baixar app</a>
+        <a href="https://www.instagram.com/luiggi_sales/" className='social' target={'_blank'} rel="noreferrer">
             <BsInstagram size={24} color='#fff'/>
         </a>
         <Link to={'/meuslinks'} className="menu-item">
